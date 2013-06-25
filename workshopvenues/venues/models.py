a77b62ccadf578b8e352e4fdae8e4efe5060f938
Andrea Grandi <a.grandi@gmail.com>
2013-06-25 23:23:59 +0100

    Add descriptive name to Venues model

--- workshopvenues/venues/models.py
+++ workshopvenues/venues/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Venues(models.Model):
-	name = models.CharField(max_length=30)
-	website = models.CharField(max_length=50)
-	address = models.CharField(max_length=200)
-	town = models.CharField(max_length=30)
-	postcode = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
+    website = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    town = models.CharField(max_length=30)
+    postcode = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return self.name
