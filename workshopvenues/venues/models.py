e964aae5b69f184a7495f977f6bc901d1a589a1e
Andrea Grandi <a.grandi@gmail.com>
2013-06-25 22:19:30 +0100

    Add initial Venues model and first South migration

--- workshopvenues/venues/models.py
+++ workshopvenues/venues/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Venues(models.Model):
+	name = models.CharField(max_length=30)
+	website = models.CharField(max_length=50)
+	address = models.CharField(max_length=200)
+	town = models.CharField(max_length=30)
+	postcode = models.CharField(max_length=10)
