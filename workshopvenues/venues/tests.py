27f723226b2eca8cbfd4161d7993ebd78d329451
Andrea Grandi <a.grandi@gmail.com>
2013-07-18 22:17:14 +0100

    Add Venue model creation test case

--- workshopvenues/venues/tests.py
+++ workshopvenues/venues/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from .models import Address
+from .models import Address, Venue
 
 
 class ModelsTest(TestCase):
@@ -18,3 +18,21 @@ class ModelsTest(TestCase):
         a.country = 'UK'
         a.save()
         self.assertTrue(a.id >= 0)
+
+    def test_create_venue(self):
+        # Create the address first
+        a = Address()
+        a.street = '23, Test Street'
+        a.town = 'London'
+        a.postcode = 'xxxxx'
+        a.country = 'UK'
+        a.save()
+
+        # Create the venue
+        v = Venue()
+        v.name = 'Venue Test'
+        v.website = 'www.myvenue.com'
+        v.address = a
+        v.save()
+        self.assertEqual(v.address.town, 'London')
+        self.assertTrue(v.id >= 0)
