86deef967af1ed4c329d98ff2c532efbfec9e29c
Andrea Grandi <a.grandi@gmail.com>
2013-08-06 21:55:35 +0100

    Add first template

--- workshopvenues/venues/views.py
+++ workshopvenues/venues/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from venues.models import Venue
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the venues index.")
+    venues = Venue.objects.all()
+    context = {'venues': venues }
+    return render(request, 'venues/index.html', context)
