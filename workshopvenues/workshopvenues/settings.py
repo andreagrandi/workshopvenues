59fb3c82d0f3e3b5290d76941e3b865299ee662b
Andrea Grandi <a.grandi@gmail.com>
2013-08-09 07:23:39 -0400

    Static files settings moved to secrets.py

--- workshopvenues/workshopvenues/settings.py
+++ workshopvenues/workshopvenues/settings.py
@@ -65,19 +65,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = secrets.static_root
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/assets/'
+STATIC_URL = secrets.static_url
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'assets'),
-)
+STATICFILES_DIRS = secrets.staticfiles_dirs
 
 # List of finder classes that know how to find static files in
 # various locations.
