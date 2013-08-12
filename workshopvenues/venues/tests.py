f682297187366f6bbfe7c8b06c3c9af573ec6e60
Valerio <valerio.delmeglio@gmail.com>
2013-08-12 23:38:51 +0200

    Commit test.py

--- workshopvenues/venues/tests.py
+++ workshopvenues/venues/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from .models import Address, Venue, Facility
+from .models import Address, Venue, Facility, Image
 
 
 class ModelsTest(TestCase):
@@ -59,3 +59,42 @@ class ModelsTest(TestCase):
         v.facilities.add(fac_elevator)
         self.assertEqual(v.address.town, 'London')
         self.assertTrue(v.id >= 0)
+    
+    def test_create_image(self):
+        # Create the address first
+        a = Address()
+        a.street = '23, Test Street'
+        a.town = 'London'
+        a.postcode = 'xxxxx'
+        a.country = 'UK'
+        a.save()  
+        
+        fac_wifi = Facility()
+        fac_wifi.name = 'WiFi'
+        fac_wifi.save()
+        self.assertTrue(fac_wifi.id >= 0)
+
+        fac_elevator = Facility()
+        fac_elevator.name = 'Elevator'
+        fac_elevator.save()
+        self.assertTrue(fac_elevator.id >= 0)
+              
+        # Create the Venue 
+        v = Venue()
+        v.name = 'Venue Test'
+        v.website = 'www.myvenue.com'
+        v.address = a
+        v.save() 
+        v.facilities.add(fac_wifi)
+        v.facilities.add(fac_elevator)
+        
+        # Create the image
+        i = Image()
+        i.filename = '/test/image/path'
+        i.venue = v
+        i.save()
+        self.assertEqual(i.venue.name, 'Venue Test')        
+        self.assertTrue(i.id >= 0) 
+        
+                     
+            
