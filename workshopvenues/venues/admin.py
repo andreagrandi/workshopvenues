1202c5cd52b60dc02c9d148286a62a86259302ee
Andrea Grandi <a.grandi@gmail.com>
2013-08-13 21:56:49 +0100

    Add Image model to admin panel

--- workshopvenues/venues/admin.py
+++ workshopvenues/venues/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from venues.models import Venue, Address, Facility
+from venues.models import Venue, Address, Facility, Image
 
 admin.site.register(Venue)
 admin.site.register(Address)
 admin.site.register(Facility)
+admin.site.register(Image)
