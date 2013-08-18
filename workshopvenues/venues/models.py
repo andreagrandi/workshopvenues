e4a49bd45bacc30362ec806f93a2ccbe83e64fec
Andrea Grandi <a.grandi@gmail.com>
2013-08-18 16:36:32 +0100

    Make it possible to set many Venue field as null

--- workshopvenues/venues/models.py
+++ workshopvenues/venues/models.py
@@ -29,21 +29,21 @@ class City(models.Model):
         verbose_name_plural = "cities"
 
 class Venue(models.Model):
-    name = models.CharField(max_length=30)
-    website = models.CharField(max_length=50, blank=True)
-    address = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=50)
+    website = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=200, blank=True, null=True)
     city = models.ForeignKey(City, null=True, blank=True)
-    postcode = models.CharField(max_length=10, blank=True)
+    postcode = models.CharField(max_length=10, blank=True, null=True)
     country = models.ForeignKey(Country, null=True, blank=True)
     facilities = models.ManyToManyField(Facility, null=True, blank=True)
-    style = models.CharField(max_length=200, blank=True)
-    twitter = models.CharField(max_length=200, blank=True)
-    phone = models.CharField(max_length=30, blank=True)
-    contact = models.CharField(max_length=50, blank=True)
-    contact_email = models.CharField(max_length=50, blank=True)
-    contact_twitter = models.CharField(max_length=200, blank=True)
-    cost = models.CharField(max_length=200, blank=True)
-    capacity = models.CharField(max_length=200, blank=True)
+    style = models.CharField(max_length=200, blank=True, null=True)
+    twitter = models.CharField(max_length=200, blank=True, null=True)
+    phone = models.CharField(max_length=30, blank=True, null=True)
+    contact = models.CharField(max_length=50, blank=True, null=True)
+    contact_email = models.CharField(max_length=50, blank=True, null=True)
+    contact_twitter = models.CharField(max_length=200, blank=True, null=True)
+    cost = models.CharField(max_length=200, blank=True, null=True)
+    capacity = models.CharField(max_length=200, blank=True, null=True)
 
     def __unicode__(self):
         return self.name
