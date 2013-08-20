4c96a061502c18dd2a051b7827adeb487ce7348f
Andrea Grandi <a.grandi@gmail.com>
2013-08-20 22:16:27 +0100

    Update instructions to run tests

--- workshopvenues/venues/tests/test_models.py
+++ workshopvenues/venues/tests/test_models.py
@@ -1,6 +1,6 @@
 """
 This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+when you run "./manage.py test --settings=workshopvenues.settings_test venues"
 
 Replace this with more appropriate tests for your application.
 """
