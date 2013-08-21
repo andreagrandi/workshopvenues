9dbe1e807687a4349c961e52af675a1ec4c40737
Andrea Grandi <a.grandi@gmail.com>
2013-08-21 23:03:48 +0100

    Revert settings to use env variables for static files

--- workshopvenues/workshopvenues/settings.py
+++ workshopvenues/workshopvenues/settings.py
@@ -79,17 +79,14 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-#STATIC_ROOT = get_env_variable("WORKSHOPVENUES_STATIC_ROOT")
-STATIC_ROOT = ''
+STATIC_ROOT = get_env_variable("WORKSHOPVENUES_STATIC_ROOT")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-#STATIC_URL = get_env_variable("WORKSHOPVENUES_STATIC_URL")
-STATIC_URL = '/static/'
+STATIC_URL = get_env_variable("WORKSHOPVENUES_STATIC_URL")
 
 # Additional locations of static files
-#STATICFILES_DIRS = (os.path.join(PROJECT_PATH, get_env_variable("WORKSHOPVENUES_STATICFILES_ASSETS")),)
-STATICFILES_DIRS = (os.path.join(PROJECT_PATH, "static"),)
+STATICFILES_DIRS = (os.path.join(PROJECT_PATH, get_env_variable("WORKSHOPVENUES_STATICFILES_ASSETS")),)
 
 # List of finder classes that know how to find static files in
 # various locations.
