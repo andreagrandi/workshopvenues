169a4eb9a8a9680896cc1c5fdd2ff07a56584d40
Andrea Grandi <a.grandi@gmail.com>
2013-08-22 21:30:02 +0100

    Add empty About page and refactor base template

--- workshopvenues/venues/views.py
+++ workshopvenues/venues/views.py
@@ -5,3 +5,6 @@ def index(request):
     venues = Venue.objects.all()
     context = {'venues': venues }
     return render(request, 'venues/index.html', context)
+
+def about(request):
+	return render(request, 'venues/about.html')
