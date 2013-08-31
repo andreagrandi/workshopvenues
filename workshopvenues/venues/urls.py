fd08fb8512d7f0d281de12ded52057d33841c4cb
Andrea Grandi <a.grandi@gmail.com>
2013-08-31 12:50:33 +0200

    Changes to initial Venue detail view

--- workshopvenues/venues/urls.py
+++ workshopvenues/venues/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from venues import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<venue_id>\d+)/$', views.detail, name='detail')
 )
