ca6cc35e62d9b8c6df570f914efea2b3a7d45534
Andrea Grandi <a.grandi@gmail.com>
2013-11-26 22:18:26 +0000

    Refactory Venue tests to use factory-boy

--- workshopvenues/venues/tests.py
+++ workshopvenues/venues/tests.py
@@ -19,6 +19,27 @@ class CityFactory(factory.DjangoModelFactory):
     FACTORY_FOR = City
     country = factory.SubFactory(CountryFactory)
 
+class VenueFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Venue
+    country = factory.SubFactory(CountryFactory)
+    city = factory.SubFactory(CityFactory)
+    name = 'Vanue Test'
+    website = 'www.myvenue.com'
+    address = '23, Test Street'
+    postcode = 'xxxxx'
+    active = True
+
+    @factory.post_generation
+    def facilities(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of facilities were passed in, use them
+            for facility in extracted:
+                self.facilities.add(facility)
+
 class ModelsTest(TestCase):
     def test_create_facility(self):
         fac_wifi = FacilityFactory.create(name = 'WiFi')
@@ -37,44 +58,9 @@ class ModelsTest(TestCase):
         self.assertTrue(city.id >= 0)
 
     def test_create_venue(self):
-        # Create the Country first
-        country = Country()
-        country.name = "United Kingdom"
-        country.save()
-
-        # Create the City
-        city = City()
-        city.name = "London"
-        city.country = country
-        city.save()
-        self.assertTrue(city.id >= 0)
-
-        # Create facilities
-        fac_wifi = Facility()
-        fac_wifi.name = 'WiFi'
-        fac_wifi.save()
-        self.assertTrue(fac_wifi.id >= 0)
-
-        fac_elevator = Facility()
-        fac_elevator.name = 'Elevator'
-        fac_elevator.save()
-        self.assertTrue(fac_elevator.id >= 0)
-
-        # Create the venue
-        v = Venue()
-        v.name = 'Venue Test'
-        v.website = 'www.myvenue.com'
-        v.street = '23, Test Street'
-        v.postcode = 'xxxxx'
-        v.country = country
-        v.city = city
-        v.active = True
-        v.save()
-        v.facilities.add(fac_wifi)
-        v.facilities.add(fac_elevator)
-        self.assertEqual(v.city.name, 'London')
-        self.assertEqual(v.country.name, 'United Kingdom')
-        self.assertTrue(v.id >= 0)
+        venue = VenueFactory.create(facilities = (FacilityFactory(name = 'WiFI'), 
+            FacilityFactory.create(name = 'Elevator')))
+        self.assertTrue(venue.id >= 0)
     
     def test_create_image(self):
         # Create the Country first
