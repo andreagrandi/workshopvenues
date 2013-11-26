b7fa3a1fbc0977813fc8e92be6d8a4b511d299d3
Andrea Grandi <a.grandi@gmail.com>
2013-11-26 23:17:20 +0000

    Do not use South migrations when executing tests and using Sqlite

--- workshopvenues/workshopvenues/settings_test.py
+++ workshopvenues/workshopvenues/settings_test.py
@@ -2,3 +2,12 @@ from settings import *
 
 TEST_RUNNER = "discover_runner.DiscoverRunner"
 TEST_DISCOVER_PATTERN = "test_*"
+SOUTH_TESTS_MIGRATE = False # To disable migrations and use syncdb instead
+SKIP_SOUTH_TESTS = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        }
+    }
